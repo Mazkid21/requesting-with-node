64370a289a8fe510f1a6e7b55fdee1f1dba7f5f7
Mazkid21 <zmaraziti@yahoo.com>
2017-07-20 21:05:09 -0600

    fixing moive.js

--- movie.js
+++ movie.js
@@ -9,31 +9,27 @@ const movieTitle = [];
 var apiUrl = "https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=012524686640135050800:_1j6pzftrqe&q=" + movieTitle;
 console.log(apiUrl);
 
-function myRequest(apiUrl, querryString) {
-
-	request(apiUrl, function (error, response, body) {
-
-		var bodyObject = JSON.parse(body);
-		console.log(bodyObject);
-
-	});
-}
-
-
-
 
 
 function get(movieTitle) {
 
 	var queryArray = movieTitle.split(" ");
-	var querryString = "";
+	var queryString = "";
 		queryArray.forEach(function(element, index) {
-		querryString += element;
+		queryString += element;
 		if (index < queryArray.length -1)
-				querryString += '+';
+				queryString += '+';
 
 });
-myRequest(apiUrl, querryString);
+
+		apiUrl += queryString;
+
+request(apiUrl, function (error, response, body) {
+
+		var bodyObject = JSON.parse(body);
+		console.log(bodyObject);
+
+	});
 
 }
 
